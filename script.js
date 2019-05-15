5d3417818ed619248c5d520505abf6fd1026b125
Katherine Peterson <katherine.peterson@yahoo.com>
2019-05-14 19:33:20 -0700

    finished html outline

---
+++ script.js
@@ -0,0 +1,26 @@
+const data = {
+    customers: {
+      Moe: {
+        Foo: 0,
+        Bar: 0,
+        Bazz: 0
+      },
+      Larry: {
+        Foo: 0,
+        Bar: 0,
+        Bazz: 0
+      },
+      Curly: {
+        Foo: 0,
+        Bar: 0,
+        Bazz: 0
+      }
+    },
+    prizes: {
+      Foo: 1,
+      Bar: 3,
+      Bazz: 5
+    }
+  };
+
+  
