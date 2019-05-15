2a5a989f91281dae0b4c78aedc6917ce1315a0ee
Katherine Peterson <katherine.peterson@yahoo.com>
2019-05-14 21:42:53 -0700

    done

--- script.js
+++ script.js
@@ -52,10 +52,13 @@ const data = {
   const customerPrizes = Array.from(document.getElementsByClassName('customer-count'));
   const actualCustomerNames = customerNames.map(function(el){return el.innerHTML});
   function renderCustomerPrizes() {
-    custHtml = `<ul> ${actualPrizeNames.map(function(el, i){return `<li><button>-</button>${el} ${data.customers[actualCustomerNames[i]][el]}<button>+</button></li>`}).join('')} </ul>`;
-    customerPrizes.forEach(function(el){
-        el.innerHTML = custHtml
-    });
+    customerPrizes[0].innerHTML = `<ul> ${actualPrizeNames.map(function(el, i){return `<li class='${i}'><button id='minus' class='${i}'>-</button>${el} ${data.customers[actualCustomerNames[0]][el]}<button id='plus' class='${i}'>+</button></li>`}).join('')} </ul>`;
+    customerPrizes[1].innerHTML = `<ul> ${actualPrizeNames.map(function(el, i){return `<li class='${i}'><button id='minus' class='${i}'>-</button>${el} ${data.customers[actualCustomerNames[1]][el]}<button id='plus' class='${i}'>+</button></li>`}).join('')} </ul>`;
+    customerPrizes[2].innerHTML = `<ul> ${actualPrizeNames.map(function(el, i){return `<li class='${i}'><button id='minus' class='${i}'>-</button>${el} ${data.customers[actualCustomerNames[2]][el]}<button id='plus' class='${i}'>+</button></li>`}).join('')} </ul>`;
+    // custHtml = `<ul> ${actualPrizeNames.map(function(el, i){return `<li class='${i}'><button id='minus' class='${i}'>-</button>${el} ${data.customers[actualCustomerNames[i]][el]}<button id='plus' class='${i}'>+</button></li>`}).join('')} </ul>`;
+    // customerPrizes.forEach(function(el){
+    //     el.innerHTML = custHtml
+    // });
   }
 
   renderCustomerPrizes();
@@ -68,3 +71,70 @@ function renderPrizeInventory() {
 }
 
 renderPrizeInventory();
+
+cust1 = document.getElementById('cust1');
+cust2 = document.getElementById('cust2');
+cust3 = document.getElementById('cust3');
+
+cust1.addEventListener('click', function(ev){
+    if (ev.target.tagName === 'BUTTON') {
+        let i = ev.target.className;
+        if (ev.target.id === 'plus' && data.prizes[actualPrizeNames[i]] > 0) {
+            data.customers[actualCustomerNames[0]][actualPrizeNames[i]] += 1;
+            data.prizes[actualPrizeNames[i]] -= 1;
+            renderCustomerPrizes();
+            renderPrizeInventory();
+        } else if (ev.target.id === 'minus'){
+            let count = data.customers[actualCustomerNames[0]][actualPrizeNames[i]];
+            if(count > 0) {
+                data.customers[actualCustomerNames[0]][actualPrizeNames[i]] -= 1;
+                data.prizes[actualPrizeNames[i]] += 1;
+            }
+            renderCustomerPrizes();
+            renderPrizeInventory();
+        }
+    }
+    console.log(data);
+});
+cust2.addEventListener('click', function(ev) {
+    if (ev.target.tagName === 'BUTTON') {
+        if (ev.target.tagName === 'BUTTON') {
+            let i = ev.target.className;
+            if (ev.target.id === 'plus' && data.prizes[actualPrizeNames[i]] > 0) {
+                data.customers[actualCustomerNames[1]][actualPrizeNames[i]] += 1;
+                data.prizes[actualPrizeNames[i]] -= 1;
+                renderCustomerPrizes();
+                renderPrizeInventory();
+            } else if (ev.target.id === 'minus'){
+                let count = data.customers[actualCustomerNames[1]][actualPrizeNames[i]];
+                if(count > 0) {
+                    data.customers[actualCustomerNames[1]][actualPrizeNames[i]] -= 1;
+                    data.prizes[actualPrizeNames[i]] += 1;
+                }
+                renderCustomerPrizes();
+                renderPrizeInventory();
+            }
+        }
+    }
+});
+cust3.addEventListener('click', function(ev) {
+    if (ev.target.tagName === 'BUTTON') {
+        if (ev.target.tagName === 'BUTTON') {
+            let i = ev.target.className;
+            if (ev.target.id === 'plus' && data.prizes[actualPrizeNames[i]] > 0) {
+                data.customers[actualCustomerNames[2]][actualPrizeNames[i]] += 1;
+                data.prizes[actualPrizeNames[i]] -= 1;
+                renderCustomerPrizes();
+                renderPrizeInventory();
+            } else if (ev.target.id === 'minus'){
+                let count = data.customers[actualCustomerNames[2]][actualPrizeNames[i]];
+                if(count > 0) {
+                    data.customers[actualCustomerNames[2]][actualPrizeNames[i]] -= 1;
+                    data.prizes[actualPrizeNames[i]] += 1;
+                }
+                renderCustomerPrizes();
+                renderPrizeInventory();
+            }
+        }
+    }
+});
